ed553be67489b354e7d35efb911420fd5c75dc44
Judy <judy.mtz1@gmail.com>
2022-03-08 01:55:16 -0600

    added require express & chained listen method to server

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 // main file where server will run from
 
 //instantiates server
+const express = require(`express`);
 const app = express();
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
