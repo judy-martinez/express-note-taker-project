cd75a9d8856024afce3460cc19c6717e23154146
Judy <judy.mtz1@gmail.com>
2022-03-08 02:08:20 -0600

    installed dependencies & updated port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@
 const express = require(`express`);
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(3003, () => {
+    console.log(`API server now on port 3003!`);
   });
