349542c972863b5acebf116e51c439fb753526ba
Judy <judy.mtz1@gmail.com>
2022-03-18 18:53:44 -0500

    added port variable

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // main file where server will run from
+const PORT = process.env.PORT || 3003;
 
 //instantiates server
 const express = require(`express`);
 const app = express();
 
-app.listen(3003, () => {
-    console.log(`API server now on port 3003!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
